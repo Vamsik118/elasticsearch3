af1ad55b3c8a2497c52e99bf0ba4cd9b58f612b8
vamsi <vamsik118@gmail.com>
2021-01-14 02:18:40 +0530

    updated final files

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 
@@ -25,8 +25,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io  -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sysctl -w vm.max_map_count=262144
-swapoff -a
-ulimit -n 65535
-sysctl -w net.ipv4.tcp_retries2=5
-mkdir -p /var/enc/db/elasticsearch3/data
+sudo sysctl -w vm.max_map_count=262144
+sudo swapoff -a
+sudo ulimit -n 65535
+sudo sysctl -w net.ipv4.tcp_retries2=5
+sudo mkdir -p /var/enc/db/elasticsearch3/data
